1f016fb45e6592ad5cac3e21a464f6b0f1872b70
12ramsake <32965328+12ramsake@users.noreply.github.com>
2018-09-09 20:47:19 -0400

    Add files via upload

---
+++ R/dpm.r
@@ -0,0 +1,21 @@
+#' Calculates the Projection Median (see Durocher 2009)
+#'
+#' This function allows you to calculate the projection median approximately or exactly in 2D.
+#' @param data a nxd matrix of your data. For two dimensions, d must be 2
+#' @param N Number of vectors to generate in the Monte Carlo algorithm
+#' @param method Which Monte Carlo algorithm to use? As per Durocher et. al 2017
+#' @param exact2D calculate the pm exactly *data must be 2 dimensions
+#' @keywords multivariate, robust, location
+#' @export
+#' @examples
+#' data<-cbind(rnorm(151),rnorm(151))
+#' dpm(data)
+#' dpm(data,exact2D=T)
+
+
+dpm<-function(data,N=10000,method=1,exact2D=F){
+  if(exact2D)
+    return(projectionMedian2D(data))
+  else
+    return(projectionMedianMC(data,N=N,method=method))
+}
